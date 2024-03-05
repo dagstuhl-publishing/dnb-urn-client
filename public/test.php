cc57abdf11fe6bf296a28a64d9f205221e42bef2
didas <didas@dagstuhl.de>
2024-03-05 12:48:38 +0100

    revised public test code

--- public/test.php
+++ public/test.php
@@ -2,8 +2,18 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$client = new LZI\DnbUrnClient\UrnClient();
+$client = new LZI\DnbUrnClient\UrnClient(
+    // 'username',
+    // 'password',
+    // 'https://api.nbn-resolving.org/sandbox/v2/'
+);
 
-var_dump($client->getNamespaceDetails('urn:nbn:de:0030'));
+var_dump(
+    $client->getNamespaceDetails('urn:nbn:de:0030'),
+    $client->getNamespaceDetails('urn:nbn:de:0030'),
+    $client->urnExists('urn:nbn:de:0030-drops-177320'),
+    $client->getUrnDetails('urn:nbn:de:0030-drops-177320'),
+    $client->getUrls('urn:nbn:de:0030-drops-177320')
+);
 
 exit();
